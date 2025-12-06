2eb5b6b90407104221508d40022827fee35d166f
Nathan-Shammami <Nathan.shammami@gmail.com>
2025-12-06 14:02:01 -0500

    navbar

---
+++ src/app/pages/navbar/components/navbar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import "./navStyles.css";
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="navLeft">
+                <h2>NS</h2>
+            </div>
+            <div className="navRight">
+                <a href="#about" className="navLinks">About</a>
+                <a href="#projects" className="navLinks">Projects</a>
+                <a href="#experience" className="navLinks">Experience</a>
+                <a href="#contact" className="navLinks">Contact</a>
+            </div>
+        </div>
+    );
+}
