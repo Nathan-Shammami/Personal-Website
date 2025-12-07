715f0ba752078ef267b1595fb1bcd5d5eb897ae0
Nathan-Shammami <Nathan.shammami@gmail.com>
2025-12-06 19:36:25 -0500

    added navbar scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import React from "react";
-import Navbar from "./pages/navbar/components/navbar";
-import Resume from "./pages/professional/components/Resume";
+import Navbar from "./pages/navbar/navbar";
+import Resume from "./pages/professional/Resume";
+import AboutMe from "./pages/about/aboutMe";
 
 export default function Home() {
   return (
     <>
     <Navbar />
-    <div className="sectionHeader">
-      <h1 className="tabHeader"> About Me </h1>
-      <h2 className="subHeader"> Who am I?</h2>
-    </div>
+
+    {/* ABOUT SECTION */}
+    <section id="about">
+      <AboutMe />
+    </section>
+
+    {/* EXPERIENCE SECTION */}
+    <section id="experience">
+      <div className="sectionHeader">
+        <h1 className="tabHeader"> Experience </h1>
+        <h2 className="subHeader"> A look into my professional Journey</h2>
+      </div>
+      <Resume />
+    </section>
+
     
-    <div className="sectionHeader">
-      <h1 className="tabHeader"> Experience </h1>
-      <h2 className="subHeader"> A look into my professional Journey</h2>
-    </div>
-    <Resume />
     </>
   );
 }
